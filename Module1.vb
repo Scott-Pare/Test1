23a644c39b69a0ebf64bc800b4396b0b65068df0
Scott-Pare <Scottinct78@gmail.com>
2015-05-31 16:15:25 -0400

    Add msg boxex
    Just added a few message boxes.

--- Module1.vb
+++ Module1.vb
@@ -1,7 +1,11 @@
 ﻿Module Module1
 
     Sub Main()
-
+        'Console.WriteLine("hello world")
+        'Console.Write("this is a very simple hello statment")
+        MsgBox("And a little msg box!")
+        MsgBox("this is a very simple hello statmente", MsgBoxStyle.Critical, "My Message to the world")
+        Console.Read()
     End Sub
 
 End Module
